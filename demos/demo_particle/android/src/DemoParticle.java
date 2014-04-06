7bc3a3d0062638889c47024177d8f4c0c7511f6a
Boris Mikic <boris.blizzard@gmail.com>
2014-04-06 12:38:46 +0000

    - demos update

--- demos/demo_particle/android/src/DemoParticle.java
+++ demos/demo_particle/android/src/DemoParticle.java
@@ -1,6 +1,6 @@
 package com.example.aprilparticle.android.demoParticle;
 
-public class DemoParticle extends net.sourceforge.april.android.Activity
+public class DemoParticle extends com.googlecode.april.android.Activity
 {
 	static
 	{
@@ -11,7 +11,7 @@ public class DemoParticle extends net.sourceforge.april.android.Activity
 	protected void onCreate(android.os.Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		this.forceArchivePath(net.sourceforge.april.android.NativeInterface.ApkPath); // forces APK as archive file
+		this.forceArchivePath(com.googlecode.april.android.NativeInterface.ApkPath); // forces APK as archive file
 	}
 	
 }
