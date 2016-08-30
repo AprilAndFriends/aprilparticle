5f8e7ca682148f176f595bf61bd6b1b3f29ca5ba
Boris Mikic <boris.blizzard@gmail.com>
2016-08-30 08:05:30 +0200

    - small update

--- demos/demo_particle/demo_particle.cpp
+++ demos/demo_particle/demo_particle.cpp
@@ -571,7 +571,7 @@ void april_init(const harray<hstr>& args)
 #if defined(_ANDROID) || defined(_IOS)
 	drawRect.setSize(april::getSystemInfo().displayResolution);
 #endif
-	april::init(april::RS_DEFAULT, april::WS_DEFAULT);
+	april::init(april::RenderSystemType::Default, april::WindowType::Default);
 	april::createRenderSystem();
 	april::Window::Options options;
 	options.fpsCounter = true;
