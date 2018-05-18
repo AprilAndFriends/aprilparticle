1b3c239d27c137136f3474715a01f5aa3b29561c
Boris Mikic <boris.blizzard@gmail.com>
2018-05-18 10:28:29 +0200

    - added initial android studio setup

---
+++ android-studio/demos/demo_aprilui/src/main/java/com/aprilparticle/demoAprilui/Main.java
@@ -0,0 +1,17 @@
+package com.aprilparticle.demoAprilui;
+
+public class Main extends com.april.Activity
+{
+	static
+	{
+		System.loadLibrary("demo_aprilui");
+	}
+	
+	@Override
+	protected void onCreate(android.os.Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		this.forceArchivePath(com.april.NativeInterface.apkPath); // forces APK as archive file
+	}
+	
+}
